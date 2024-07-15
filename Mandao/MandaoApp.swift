184b6811c30a6c3e95b72742f7c306f7353e1ec1
Yorgi <yorgidelriomarquez@gmail.com>
2024-07-15 18:30:13 -0400

    Initial Commit

---
+++ Mandao/MandaoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MandaoApp.swift
+//  Mandao
+//
+//  Created by yorgi on 7/15/24.
+//
+
+import SwiftUI
+
+@main
+struct MandaoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
